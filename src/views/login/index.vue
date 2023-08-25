9caeb4592036467121a2dc10ca255b0d54f3061f
TOP-Z\riley <riley211375@gmail.com>
2023-08-25 22:07:19 +0800

    loginpage

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,10 +1,37 @@
 <template>
-  <div>login</div>
+  <div class="login-page">
+    <el-card>
+      <template #header> 黑马面经运营后台 </template>
+      <el-form ref="form" :model="form">
+        <el-form-item label="用户名：">
+          <el-input
+            placeholder="请输入用户名"
+            v-model="form.username"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="密码：">
+          <el-input placeholder="请输入密码" v-model="form.password"></el-input>
+        </el-form-item>
+        <el-form-item class="tc">
+          <el-button type="primary">登录</el-button>
+          <el-button>重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'login-page'
+  name: 'LoginIndex',
+  data() {
+    return {
+      form: {
+        username: '',
+        password: ''
+      }
+    }
+  }
 }
 </script>
 
