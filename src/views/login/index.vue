6d87ecd55f36bea73a44b65909ce81a64a9fb2b0
TOP-Z\riley <riley211375@gmail.com>
2023-08-26 15:46:02 +0800

    Validation

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,19 +2,20 @@
   <div class="login-page">
     <el-card>
       <template #header> 黑马面经运营后台 </template>
-      <el-form ref="form" :model="form">
-        <el-form-item label="用户名：">
+      <!-- 表单校验 -->
+      <el-form ref="form" :model="form" :rules="rules">
+        <el-form-item label="用户名：" prop="username">
           <el-input
             placeholder="请输入用户名"
             v-model="form.username"
           ></el-input>
         </el-form-item>
-        <el-form-item label="密码：">
+        <el-form-item label="密码：" prop="password">
           <el-input placeholder="请输入密码" v-model="form.password"></el-input>
         </el-form-item>
         <el-form-item class="tc">
-          <el-button type="primary">登录</el-button>
-          <el-button>重置</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button @click="reset">重置</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -29,7 +30,46 @@ export default {
       form: {
         username: '',
         password: ''
+      },
+      // 校验规则
+      rules: {
+        username: [
+          {
+            required: true,
+            message: '请输入用户名',
+            trigger: ['blur', 'change']
+          },
+          {
+            min: 5,
+            max: 11,
+            message: '长度在 5 到 11 个字符',
+            trigger: ['blur', 'change']
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: '请输入密码',
+            trigger: ['blur', 'change']
+          },
+          {
+            pattern: /^\w{5,10}$/,
+            message: '长度在 5 到 11 个字符',
+            trigger: ['blur', 'change']
+          }
+        ]
       }
+    }
+  },
+  methods: {
+    login() {
+      // 兜底校验，如果校验失败,不会继续往下执行
+      this.$refs.form.validate((valid) => {
+        if (!valid) return false
+      })
+    },
+    reset() {
+      this.$refs.form.resetFields()
     }
   }
 }
