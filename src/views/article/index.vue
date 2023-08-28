84030946b8f038d4962851e558297d19513383fd
TOP-Z\riley <riley211375@gmail.com>
2023-08-28 15:15:53 +0800

    table

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,10 +1,63 @@
 <template>
-  <div>article</div>
+  <div class="article-page">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item>面经后台</el-breadcrumb-item>
+      <el-breadcrumb-item>面经管理</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card shadow="never" border="false">
+      <!-- 1.头部 -->
+      <template #header>
+        <div class="header">
+          <span>共 300 条记录</span>
+          <el-button icon="el-icon-plus" size="small" type="primary" round>
+            添加面经
+          </el-button>
+        </div>
+      </template>
+      <!-- 2.表格 -->
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column prop="date" label="日期" width="180">
+        </el-table-column>
+        <el-table-column prop="name" label="姓名" width="180">
+        </el-table-column>
+        <el-table-column prop="address" label="地址"> </el-table-column>
+      </el-table>
+      <!-- 3.分页组件 -->
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'article-page'
+  name: 'article-page',
+  data() {
+    return {
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }
+      ]
+    }
+  },
+  created() {},
+  methods: {}
 }
 </script>
 
